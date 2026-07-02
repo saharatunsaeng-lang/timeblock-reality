5e99983864688d193b471c1b3379353a98cff7ff
Saharat Unsaeng <saharatunsaeng@Saharats-MacBook-Air.local>
2026-07-02 08:32:18 +0700

    Force refresh LD8 app cache

--- sw.js
+++ sw.js
@@ -1,10 +1,44 @@
-const cacheName = "timeblock-reality-v2";
-const assets = ["./", "index.html", "styles.css", "app.js", "manifest.webmanifest", "icon.svg"];
+const cacheName = "timeblock-reality-v3";
+const assets = [
+  "./?v=20260702-ld8",
+  "index.html?v=20260702-ld8",
+  "styles.css?v=20260702-ld8",
+  "app.js?v=20260702-ld8",
+  "manifest.webmanifest?v=20260702-ld8",
+  "icon.svg",
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+  const request = event.request;
+  const isDocument = request.mode === "navigate";
+  const isVersionedAsset = new URL(request.url).searchParams.has("v");
+
+  if (isDocument || isVersionedAsset) {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(cacheName).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => caches.match(request)),
+    );
+    return;
+  }
+
+  event.respondWith(caches.match(request).then((cached) => cached || fetch(request)));
 });
